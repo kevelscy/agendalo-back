521b2981d78b43525f9bbc1ad384e7dda0e3d3c9
kevelscy <kevelscy@gmail.com>
2025-03-25 06:21:37 -0400

    add: dictionary error

--- src/config/consts/success/users.ts
+++ src/config/consts/success/users.ts
@@ -35,17 +35,17 @@ type DictionaryKeys =
 type DictionaryErrors = Record<DictionaryKeys, { message: string, code: number }>
 
 export const USER_DICTIONARY_ERRORS: DictionaryErrors = {
-  USER_CREATE_ERROR: {
+  USER_CREATE_SUCCESS: {
     message: 'No se ha pudo crear el usuario',
     code: 404
   },
 
-  USER_EXIST_ERROR: {
+  USER_DELETE_SUCCESS: {
     message: 'Ya existe este usuario',
     code: 401
   },
 
-  USER_EDIT_ERROR: {
+  USER_EDIT_SUCCESS: {
     message: 'Error al editar el usuario',
     code: 401
   },
